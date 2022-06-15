d0152a8d265b0addf3ef04d93e6c83733e0fac3c
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-06-15 15:25:42 +0900

    replace creating vue instance process from vue2 to vue3

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import 'core-js/modules/es.array.flat-map'
 import 'core-js/modules/es.object.entries'
 import 'core-js/modules/es.object.values'
 
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import VueSlider from '../lib'
 import '../lib/theme/default.scss'
 
-Vue.component('VueSlider', VueSlider)
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = createApp(App)
+app.component('VueSlider', VueSlider)
+app.config.performance = false
+app.mount('#app')
