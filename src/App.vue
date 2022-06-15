623374079ea7b43400e566e6882a99e4debf175b
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-06-15 15:26:48 +0900

    fix sample code for dropping v-model support

--- src/App.vue
+++ src/App.vue
@@ -17,97 +17,251 @@
 
         <div class="sample">
           <p>tooltip=none</p>
-          <VueSlider v-model="value1" tooltip="none" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            tooltip="none"
+          />
         </div>
 
         <div class="sample">
           <p>tooltip=always</p>
-          <VueSlider v-model="value1" tooltip="always" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            tooltip="always"
+          />
         </div>
 
         <div class="sample">
           <p>tooltipFormatter={value}%</p>
-          <VueSlider v-model="value1" :tooltip-formatter="'{value}%'" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            :tooltip-formatter="'{value}%'"
+          />
         </div>
 
         <div class="sample">
           <p>contained=true</p>
-          <VueSlider v-model="value1" contained />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            contained
+          />
         </div>
 
         <div class="sample">
           <p>dotSize=30</p>
-          <VueSlider v-model="value1" :dotSize="30" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            :dotSize="30"
+          />
         </div>
 
         <div class="sample">
           <p>width=50%</p>
-          <VueSlider v-model="value1" width="50%" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            width="50%"
+          />
         </div>
 
         <div class="sample">
           <p>height=20px</p>
-          <VueSlider v-model="value1" height="20px" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            height="20px"
+          />
         </div>
 
         <div class="sample">
           <p>direction=rtl</p>
-          <VueSlider v-model="value1" direction="rtl" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            direction="rtl"
+          />
         </div>
 
         <div class="sample">
           <p>duration=0</p>
-          <VueSlider v-model="value1" :duration="0" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            :duration="0"
+          />
         </div>
 
         <div class="sample">
           <p>lazy</p>
-          <VueSlider v-model="value1" lazy />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            lazy
+          />
         </div>
 
         <div class="sample">
           <p>direction=ttb</p>
-          <VueSlider v-model="value1" direction="ttb" height="200px" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            direction="ttb"
+            height="200px"
+          />
         </div>
 
         <div class="sample">
           <p>direction=btt</p>
-          <VueSlider v-model="value1" direction="btt" height="200px" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            direction="btt"
+            height="200px"
+          />
         </div>
 
         <div class="sample">
           <p>disabled</p>
-          <VueSlider v-model="value1" disabled />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            disabled
+          />
         </div>
 
         <div class="sample">
           <p>dotStyle</p>
-          <VueSlider v-model="value1" :dot-style="{ 'background-color': '#000' }" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            :dot-style="{ 'background-color': '#000' }"
+          />
         </div>
 
         <div class="sample">
           <p>railStyle</p>
-          <VueSlider v-model="value1" :rail-style="{ 'background-color': '#fc851b' }" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            :rail-style="{ 'background-color': '#fc851b' }"
+          />
         </div>
 
         <div class="sample">
           <p>processStyle</p>
-          <VueSlider v-model="value1" :process-style="{ 'background-color': '#fc851b' }" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            :process-style="{ 'background-color': '#fc851b' }"
+          />
         </div>
 
         <div class="sample">
           <p>tooltipStyle</p>
-          <VueSlider v-model="value1" tooltip="always" :tooltip-style="{ 'background-color': '#fc851b' }" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            tooltip="always"
+            :tooltip-style="{ 'background-color': '#fc851b' }"
+          />
         </div>
 
         <div class="sample">
           <p>style</p>
-          <VueSlider v-model="value1" :style="{ 'background-color': '#fc851b' }" />
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            :style="{ 'background-color': '#fc851b' }"
+          />
         </div>
 
         <div class="sample">
           <p>dot slot</p>
-          <VueSlider v-model="value1">
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+          >
             <template #dot>
               <span class="dot">{{ value1 }}</span>
             </template>
@@ -116,7 +270,15 @@
 
         <div class="sample">
           <p>tooltip slot</p>
-          <VueSlider v-model="value1" tooltip="always">
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            tooltip="always"
+          >
             <template #tooltip>
               <span class="tooltip">{{ value1 }}</span>
             </template>
@@ -125,7 +287,15 @@
 
         <div class="sample">
           <p>default slot</p>
-          <VueSlider v-model="value1" tooltip="always">
+          <VueSlider
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            tooltip="always"
+          >
             <template>
               <span class="tooltip">{{ value1 }}</span>
             </template>
@@ -135,10 +305,15 @@
         <div class="sample">
           <p>event (console logs)</p>
           <VueSlider
-            v-model="value1"
-            @drag-start="(v) => log('@dragStart', v)"
-            @drag-end="(v) => log('@dragEnd', v)"
-            @dragging="(v) => log('@dragging', v)"
+            :value="value1"
+            @change="
+              v => {
+                value1 = v
+              }
+            "
+            @drag-start="v => log('@dragStart', v)"
+            @drag-end="v => log('@dragEnd', v)"
+            @dragging="v => log('@dragging', v)"
           />
         </div>
       </div>
@@ -149,7 +324,17 @@
       <div class="sample-list">
         <div class="sample">
           <p>min=0 max=10 interval=0.1</p>
-          <VueSlider v-model="value2" :min="0" :max="10" :interval="0.1" />
+          <VueSlider
+            :value="value2"
+            @change="
+              v => {
+                value2 = v
+              }
+            "
+            :min="0"
+            :max="10"
+            :interval="0.1"
+          />
         </div>
       </div>
     </div>
@@ -159,12 +344,27 @@
       <div class="sample-list">
         <div class="sample">
           <p>default</p>
-          <VueSlider v-model="value3" />
+          <VueSlider
+            :value="value3"
+            @change="
+              v => {
+                value3 = v
+              }
+            "
+          />
         </div>
 
         <div class="sample">
           <p>enableCross=false</p>
-          <VueSlider v-model="value3" :enableCross="false" />
+          <VueSlider
+            :value="value3"
+            @change="
+              v => {
+                value3 = v
+              }
+            "
+            :enableCross="false"
+          />
         </div>
       </div>
     </div>
@@ -174,12 +374,32 @@
       <div class="sample-list">
         <div class="sample">
           <p>marks=true</p>
-          <VueSlider v-model="value4" :min="0" :max="10" :marks="true" />
+          <VueSlider
+            :value="value4"
+            @change="
+              v => {
+                value4 = v
+              }
+            "
+            :min="0"
+            :max="10"
+            :marks="true"
+          />
         </div>
 
         <div class="sample">
           <p>adsorb=true</p>
-          <VueSlider v-model="value4" :min="0" :max="10" :adsorb="true" />
+          <VueSlider
+            :value="value4"
+            @change="
+              v => {
+                value4 = v
+              }
+            "
+            :min="0"
+            :max="10"
+            :adsorb="true"
+          />
         </div>
       </div>
     </div>
@@ -189,11 +409,19 @@
       <div class="sample-list">
         <div class="sample">
           <p>data="['A', 'B', 'C', 'D']"</p>
-          <VueSlider v-model="value5" :data="['A', 'B', 'C', 'D']" :marks="true" />
+          <VueSlider
+            :value="value5"
+            @change="
+              v => {
+                value5 = v
+              }
+            "
+            :data="['A', 'B', 'C', 'D']"
+            :marks="true"
+          />
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -205,14 +433,14 @@ export default {
       value2: 5.0,
       value3: [20, 80],
       value4: 5,
-      value5: 'C'
+      value5: 'C',
     }
   },
   methods: {
     log(msg, value) {
       console.log({ msg, value })
-    }
-  }
+    },
+  },
 }
 </script>
 
