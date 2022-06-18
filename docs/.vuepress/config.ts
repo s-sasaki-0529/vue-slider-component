3b3afe5a4379409ce5b2f9703f41c2aba4bec517
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-06-18 14:39:18 +0900

    add basic vuepress config file

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'vue-3-slider-component',
+  description: 'A highly customized slider component for Vue 3',
+})
