6aa105c6394e0223e408de052f905cae6d1b1ec3
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-06-18 15:35:52 +0900

    Revert "add basic vuepress config file"
    This reverts commit 3b3afe5a4379409ce5b2f9703f41c2aba4bec517.

--- docs/.vuepress/config.ts
+++
@@ -1,7 +0,0 @@
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  lang: 'en-US',
-  title: 'vue-3-slider-component',
-  description: 'A highly customized slider component for Vue 3',
-})
