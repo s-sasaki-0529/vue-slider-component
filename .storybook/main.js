1f7af37fe43d6d9fcadb382d56e9e9ace4e3d263
Shingo.sasaki <shingo.sasaki.0529@gmail.com>
2022-06-20 13:12:20 +0900

    add sandbox to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   stories: [
     '../src/stories/vue-3-slider-component.stories.ts',
+    '../src/stories/sandbox.stories.ts',
     '../src/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   addons: ['@storybook/addon-essentials'],
---
+++ src/stories/sandbox.stories.ts
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/vue3'
+import { ref } from 'vue'
+import VueSlider from '../../lib'
+
+export default {
+  title: 'sandbox',
+  component: VueSlider,
+} as Meta
+
+export const sandbox: Story = args => ({
+  components: { VueSlider },
+  setup() {
+    const value = ref(args.modelValue)
+    return { value, sandbox, args }
+  },
+  template: `
+    <VueSlider v-model="value" v-bind="args">
+  `,
+})
+sandbox.storyName = 'sandbox'
+sandbox.args = {
+  modelValue: 50,
+}
