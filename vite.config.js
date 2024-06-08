4f763d1e1b7a96d25cdfc64256acf9f7d3c2ebda
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2024-06-08 18:35:28 +0900

    chore: fix build config for vite

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     extensions: ['.js', '.ts', '.vue']
+  },
+  build: {
+    lib: {
+      entry: 'lib/index.js',
+      name: 'index',
+    },
   }
 })
